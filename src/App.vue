9ec52311784da7d716d95cdb6990b9f4ed66d359
Maks Kolman <kolman.maks@gmail.com>
2020-07-14 23:06:22 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-app id="app">
+    <v-app-bar
+      raised
+    >
+      <v-img
+        :src="require('./assets/logo.png')"
+        contain
+        width="200"
+        height="50"
+        max-width="500"
+        max-height="300"
+      ></v-img>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </v-app-bar>
+
+    <v-main>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <router-view/>
+      </v-container>
+    </v-main>
+    <v-footer
+      color="white"
+      app
+    >
+      <span class="white--text">&copy; 2020</span>
+    </v-footer>
+    <v-snackbar bottom right :value="updateExists" :timeout="-1" color="primary">
+      An update is available
+      <v-btn text @click="refreshApp">
+        Update
+      </v-btn>
+    </v-snackbar>
+  </v-app>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
+
+<script>
+import update from './mixins/update';
+
+export default {
+  name: 'App',
+  mixins: [update],
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <img
+      alt="Vue logo"
+      :src="require('../assets/logo.svg')"
+      class="my-3"
+      contain
+      height="200">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+  },
+};
+</script>
