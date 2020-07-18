689a1a4b202a2b12c8a1673c4679cfa70b3be011
Maks Kolman <makskolman@google.com>
2020-07-18 11:04:55 +0100

    Change update available snackbar font and colors

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,36 @@
     <v-main>
       <router-view class="view" />
     </v-main>
-    <v-snackbar bottom right :value="updateExists" :timeout="-1" color="primary">
+    <v-snackbar
+      id="updateSnack"
+      bottom
+      right
+      :value="updateExists"
+      :timeout="-1"
+    >
       An update is available
-      <v-btn text @click="refreshApp">
+      <template v-slot:action="{ attrs }">
+      <v-btn
+        text
+        color="var(--color-speak-view-btn-bg)"
+        v-bind="attrs"
+        @click="refreshApp"
+      >
         Update
       </v-btn>
+      </template>
     </v-snackbar>
   </v-app>
 </template>
 
 <style lang="scss">
 @import "./styles/_colors.scss";
+#updateSnack {
+  font-family: 'Do Hyeon', sans-serif;
+  .v-snack__content {
+    color: var(--color-speak-view-bg)!important;
+  }
+}
 #app {
   font-family: 'Galada', cursive;
   -webkit-font-smoothing: antialiased;
