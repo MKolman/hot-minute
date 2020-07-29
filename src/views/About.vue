77a471f309b5854bfdb5fbdc4ca330e2ba0834d0
Maks Kolman <makskolman@google.com>
2020-07-29 20:11:04 +0100

    Add credits to the about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -9,14 +9,12 @@
     </h1>
 
     <p>
-
       Hot Minute is a game app inspiried by <a
       href="https://boardgamegeek.com/boardgame/8790/activity">Activity</a>
       and <a href="https://www.amazon.com/dp/178905642X">Fabulous After Dinner
       Games For Not So Responsible Adults</a>. It's a party game played in
       teams of at least two people. Players take turns explaining, miming, or
       drawing objects and phrases, as well humming melodies to well known songs.
-
     </p>
 
     <h2> Installation </h2>
@@ -37,11 +35,30 @@
       on github</a> under a GPLv3 license.
     </p>
 
+    <h2>Contact</h2>
+
     <p>
       If you find an issue with app or any of the words present you can either
       <a href="mailto:maks@kolman.si">email us</a> with the details or <a
       href="https://github.com/MKolman/hot-minute/issues">open an issue on
       github</a>.
+    </p>
+
+    <h2>Credits</h2>
+
+    <p>
+      In the making of this app the following resourcers have been utilized:
+      <ul>
+        <li><a href="https://github.com/dangelov/Pictionary-Card-Generator">
+          Dangelov's pictionary clues
+        </a></li>
+        <li><a href="https://www.zapsplat.com/">
+          Zapsplat's free library of sound effects
+        </a></li>
+        <li><a href="https://www.billboard.com/">
+          Billboard lists of most popular songs
+        </a></li>
+      </ul>
     </p>
 
     <HomeFab />
