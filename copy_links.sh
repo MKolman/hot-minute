c73536a769e0fb88fd142ae5c97715ed2108f2b1
Maks Kolman <makskolman@google.com>
2021-01-01 22:35:54 +0000

    Create html fallbacks with vue

--- copy_links.sh
+++
@@ -1,7 +0,0 @@
-from=$1
-to=$2
-for f in $(find $1 -type l); do
-	target="${f/$1/$2}";
-	echo "Copying $f to $target";
-	cp $f $target -a;
-done
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,53 @@
 const routes = require('./src/router/routes');
 
 module.exports = {
+  pages: {
+    index: {
+      entry: 'src/main.ts',
+      template: 'public/index.html',
+      filename: 'index.html',
+    },
+    bomb: {
+      entry: 'src/main.ts',
+      template: 'public/index.html',
+      filename: 'play/bomb.html',
+    },
+    draw: {
+      entry: 'src/main.ts',
+      template: 'public/index.html',
+      filename: 'play/draw.html',
+    },
+    show: {
+      entry: 'src/main.ts',
+      template: 'public/index.html',
+      filename: 'play/show.html',
+    },
+    speak: {
+      entry: 'src/main.ts',
+      template: 'public/index.html',
+      filename: 'play/speak.html',
+    },
+    rules: {
+      entry: 'src/main.ts',
+      template: 'public/index.html',
+      filename: 'rules.html',
+    },
+    settings: {
+      entry: 'src/main.ts',
+      template: 'public/index.html',
+      filename: 'settings.html',
+    },
+    import: {
+      entry: 'src/main.ts',
+      template: 'public/index.html',
+      filename: 'import.html',
+    },
+    about: {
+      entry: 'src/main.ts',
+      template: 'public/index.html',
+      filename: 'about.html',
+    },
+  },
   publicPath: '/',
   transpileDependencies: [
     'vuetify',
@@ -59,12 +106,12 @@ module.exports = {
     }
   },
   chainWebpack: (config) => {
-    config
-      .plugin('html')
-      .tap((args) => {
-        args[0].title = 'Hot Minute';
-        return args;
-      });
+    // config
+    //   .plugin('html')
+    //   .tap((args) => {
+    //     args[0].title = 'Hot Minute';
+    //     return args;
+    //   });
 
     config
       .module
